830b6ac475151915a5b740e19f3f0b60886baa1d
Charlie Azmitia <ccazmitia@gmail.com>
2019-05-25 13:22:39 -0400

    initial setup with Eliots suggestions

--- server/server.js
+++ server/server.js
@@ -4,9 +4,7 @@ const path = require('path');
 
 app.use(express.json());
 
-app.get('/app.js', (req, res, next) =>
-  res.sendFile(path.join(__dirname, '..', 'dist', 'main.js'))
-);
+app.use(express.static(path.join(__dirname, '..', 'dist')));
 
 app.get('/', (req, res, next) =>
   res.sendFile(path.join(__dirname, 'index.html'))
