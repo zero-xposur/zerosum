4aaad9abaa9fdf3c37ec31408b0aee2ee90da108
Bailie Wu <wubailie@gmail.com>
2019-05-26 01:10:15 -0400

    seed beers & breweries

--- db/seed.js
+++ db/seed.js
@@ -1,20 +1,64 @@
 const { connection } = require('./db')
+// csv
 const csv = require('jquery-csv')
 const beerCSV = require('./csv/beers')
 const breweriesCSV = require('./csv/breweries')
 const stylesCSV = require('./csv/styles')
 const categoriesCSV = require('./csv/categories')
-
+// models
+const users = require('./models/users')
+const beers = require('./models/beers')
+const breweries = require('./models/breweries')
+const categories = require('./models/categories')
+const styles = require('./models/styles')
+// csv parsing
 const beersList = csv.toObjects(beerCSV)
 const breweriesList = csv.toObjects(breweriesCSV)
 const stylesList = csv.toObjects(stylesCSV)
 const categoriesList = csv.toObjects(categoriesCSV)
 
 // console.log(csv.toObjects(beerCSV)[1])
-console.log(Object.keys(categoriesList[1]))
+// console.log(Object.keys(categoriesList[1]))
 
-// connection
-//   .sync({
-//     force: true
-//   })
-//   .then(() => {})
+connection
+  .sync({
+    force: true
+  })
+  .then(() => {
+    return users.create({
+      name: 'testUser1'
+    })
+  })
+  .then(() => {
+    return Promise.all(
+      beersList.map(beer =>
+        beers.create({
+          id: beer.id,
+          brewery_id: beer.brewery_id,
+          name: beer.name,
+          cat_id: beer.cat_id,
+          style_id: beer.style_id,
+          abv: beer.abv,
+          descript: beer.descript
+        })
+      )
+    )
+  })
+  .then(() => {
+    return Promise.all(
+      breweriesList.map(brewery =>
+        breweries.create({
+          name: brewery.name,
+          address1: brewery.address1,
+          address2: brewery.address2,
+          city: brewery.city,
+          state: brewery.state,
+          code: brewery.code,
+          country: brewery.country,
+          phone: brewery.phone,
+          website: brewery.website,
+          descript: brewery.descript
+        })
+      )
+    )
+  })
