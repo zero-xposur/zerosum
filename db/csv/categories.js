d9d01beffe7859d75e7dda22f6e551dec662130e
Bailie Wu <wubailie@gmail.com>
2019-05-25 23:59:50 -0400

    csv

---
+++ db/csv/categories.js
@@ -0,0 +1,14 @@
+const categoriesCSV = `id,cat_name,last_mod
+1,British Ale,10/24/2010 13:50
+2,Irish Ale,6/8/2010 0:00
+3,North American Ale,6/8/2010 0:00
+4,German Ale,6/8/2010 0:00
+5,Belgian and French Ale,6/8/2010 0:00
+6,International Ale,6/8/2010 0:00
+7,German Lager,6/8/2010 0:00
+8,North American Lager,6/8/2010 0:00
+9,Other Lager,6/8/2010 0:00
+10,International Lager,6/8/2010 0:00
+11,Other Style,6/8/2010 0:00`
+
+module.exports = categoriesCSV
