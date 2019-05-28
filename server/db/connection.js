b09e154a674f326dd87bcabff9f648cd89487cd0
Bailie Wu <wubailie@gmail.com>
2019-05-27 23:20:45 -0400

    db updates

---
+++ server/db/connection.js
@@ -0,0 +1,9 @@
+const Sequelize = require('sequelize')
+
+const dbName = process.env.DATABASE_URL || 'postgres://localhost:5432/zerosum'
+
+const connection = new Sequelize(dbName, {
+  //   logging: false
+})
+
+module.exports = { connection, Sequelize }
--- server/db/index.js
+++ server/db/index.js
@@ -1,10 +1,4 @@
-const Sequelize = require('sequelize')
-
-const dbName = process.env.DATABASE_URL || 'postgres://localhost:5432/zerosum'
-
-const connection = new Sequelize(dbName, {
-  //   logging: false
-})
+const { Sequelize, connection } = require('./connection')
 
 const sync = () => {
   return new Promise((res, rej) => {
