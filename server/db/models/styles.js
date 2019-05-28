c5c70b20fda42b856a51929c6100b1ce1d4410ff
Bailie Wu <wubailie@gmail.com>
2019-05-28 18:30:58 -0400

    prep associations

--- server/db/models/styles.js
+++ server/db/models/styles.js
@@ -1,10 +1,8 @@
 const { connection, Sequelize } = require('..')
 
 const Styles = connection.define('style', {
-  cat_id: Sequelize.INTEGER,
+  // cat_id: Sequelize.INTEGER,
   style_name: Sequelize.TEXT
 })
-
-// [ 'id', 'cat_id', 'style_name', 'last_mod' ]
 
 module.exports = Styles
