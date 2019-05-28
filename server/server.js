1252e01fe6b2d9d1e590ca707b434bdb8bf0a237
ami-domadia <adomadia9@gmail.com>
2019-05-28 18:36:58 -0400

    adding express session

--- server/server.js
+++ server/server.js
@@ -1,13 +1,46 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const morgan = require('morgan');
+const session = require('express-session');
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '..', 'dist')));
 
+// Session middleware
+app.use(session({
+  secret: 'This is not a very secure secret...',
+  resave: false,
+  saveUninitialized: false
+}));
+
+// authentication router
+
+app.use('/auth', require('./routes/user'));
+
 app.get('/', (req, res, next) =>
   res.sendFile(path.join(__dirname, 'index.html'))
 );
+
+// Handle 404s
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((error, req, res, next) => {
+  let errors = [error];
+  if (error.errors) {
+    error = error.errors.map(_error => {
+      return _error.message;
+    });
+  } else if (error.original) {
+    errors = [error.original.message];
+  }
+  res.status(error.status || 500).send({ errors });
+});
 
 module.exports = app;
