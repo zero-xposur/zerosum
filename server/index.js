a245a169c05e4587fcb0f5c2d6ec943e0da6401f
Charlie Azmitia <ccazmitia@gmail.com>
2019-05-30 00:37:41 -0400

    smallchange

--- server/index.js
+++ server/index.js
@@ -1,8 +1,5 @@
 const server = require('./server');
-const { sync } = require('./db');
 
 const port = process.env.PORT || 3000;
-
-sync();
 
 server.listen(port, () => console.log(`listening on port ${port}`));
