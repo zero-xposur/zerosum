97c5ff6dd367199129b312fd71991639b39a2051
Bailie Wu <wubailie@gmail.com>
2019-05-29 23:11:55 -0400

    seed. also git removed large file

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,8 @@
 const { Sequelize, connection } = require('./connection')
 // import models
-const Beers = require('./models/beers')
-const Breweries = require('./models/breweries')
-const Categories = require('./models/categories')
-const Styles = require('./models/styles')
 const Users = require('./models/users')
 
 // associations
-Styles.belongsTo(Categories, { as: 'cat_id' })
-Categories.hasMany(Styles, { as: 'cat_id' })
 
 const sync = () => {
   return new Promise((res, rej) => {
--- server/db/seed.js
+++ server/db/seed.js
@@ -1,91 +1,37 @@
-const { connection } = require('.')
-// csv
-const csv = require('jquery-csv')
-const beerCSV = require('./csv/beers')
-const breweriesCSV = require('./csv/breweries')
-const stylesCSV = require('./csv/styles')
-const categoriesCSV = require('./csv/categories')
+const { connection } = require('./connection')
+const Sequelize = require('sequelize')
+
 // models
 const Users = require('./models/users')
-const Beers = require('./models/beers')
-const Breweries = require('./models/breweries')
-const Categories = require('./models/categories')
-const Styles = require('./models/styles')
-// csv parsing
-const beersList = csv.toObjects(beerCSV)
-const breweriesList = csv.toObjects(breweriesCSV)
-const stylesList = csv.toObjects(stylesCSV)
-const categoriesList = csv.toObjects(categoriesCSV)
 
-connection
-  .sync({
-    force: true
-  })
-  .then(() => {
-    return Users.create({
-      name: 'testUser1'
-    })
-  })
-  .then(() => {
-    return Promise.all(
-      beersList.map(beer =>
-        Beers.create({
-          id: beer.id,
-          brewery_id: beer.brewery_id,
-          name: beer.name,
-          cat_id: beer.cat_id,
-          style_id: beer.style_id,
-          abv: beer.abv,
-          descript: beer.descript
-        })
-      )
-    )
-  })
-  .then(() => {
-    return Promise.all(
-      breweriesList.map(brewery =>
-        Breweries.create({
-          name: brewery.name,
-          address1: brewery.address1,
-          address2: brewery.address2,
-          city: brewery.city,
-          state: brewery.state,
-          code: brewery.code,
-          country: brewery.country,
-          phone: brewery.phone,
-          website: brewery.website,
-          descript: brewery.descript
-        })
-      )
-    )
-  })
-  .then(() => {
-    return Promise.all(
-      categoriesList.map(category =>
-        Categories.create({
-          cat_name: category.cat_name
-        })
-      )
-    )
-  })
-  .then(() => {
-    return Promise.all(
-      stylesList.map(style =>
-        Styles.create({
-          cat_id: style.cat_id,
-          style_name: style.style_name
-        })
-      )
-    )
-  })
-  .then(() => {
-    return Beers.findAll({
-      where: {
-        id: 1
-      },
-      includes: [{ model: Breweries }]
-    })
-  })
-  .then(result => {
-    result.map(t => console.log(t.get()))
-  })
+// COPY public.babeers (id, name, brewery, "breweryId", link, style, abv, ratings, score, "createdAt", "updatedAt") FROM stdin;
+// 1	1842	Plzeňský Prazdroj	1	https://www.beeradvocate.com/beer/profile/1/279167/	Bohemian Pilsener	4.40000000000000036	3	3.79999999999999982	2019-05-27 01:06:56.507-04	2019-05-27 01:06:56.507-04
+
+const Babeers = connection.define('babeer', {
+  name: Sequelize.TEXT,
+  brewery: Sequelize.TEXT,
+  breweryId: Sequelize.TEXT,
+  link: Sequelize.TEXT,
+  style: Sequelize.TEXT,
+  abv: Sequelize.TEXT,
+  ratings: Sequelize.TEXT,
+  score: Sequelize.TEXT
+})
+
+// COPY public.prices (id, name, image, link, price, tags, description, quantity, "createdAt", "updatedAt") FROM stdin;
+// 33097	Kona Castaway IPA	https://www.totalwine.com/media/sys_master/twmmedia/h51/h29/9687617339422.png	https://www.totalwine.com/beer/ale/ipa-india-pale-ale-/american-ipa/kona-castaway-ipa/p/137098901?s=909&igrules=true	159.990000000000009	{"United States",Hawaii,Ale,"IPA (India Pale Ale)","American IPA"}	Hawaii- American India Pale Ale (IPA)- 6.0% ABV. Kona Castaway, part of Kona's Aloha Series, is a uniquely Hawaiian IPA balanced with delightful tropical fruit notes. It can...	1/2 Keg	2019-05-26 21:10:22.068-04	2019-05-26 21:10:22.068-04
+
+const Prices = connection.define('price', {
+  name: Sequelize.TEXT,
+  image: Sequelize.TEXT,
+  link: Sequelize.TEXT,
+  price: Sequelize.TEXT,
+  tags: Sequelize.TEXT,
+  description: Sequelize.TEXT,
+  quantity: Sequelize.TEXT
+})
+
+connection.sync({ force: true })
+
+// old json
+// "seed": "psql zerosum < server/db/beerAppData.sql",
