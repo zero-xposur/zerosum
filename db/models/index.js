bad4ba32afc4501976c7bf97a7c43b13ed97e39c
Bailie Wu <wubailie@gmail.com>
2019-06-01 20:47:58 -0400

    fix syncAndSeed

---
+++ db/models/index.js
@@ -0,0 +1,11 @@
+const Babeers = require('./babeers');
+const Price = require('./price');
+const User = require('./users');
+const Userratings = require('./userRatings');
+
+module.exports = {
+    Babeers,
+    Price,
+    User,
+    Userratings,
+};
