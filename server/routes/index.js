38be720e0056b53be6524210ecc69c64eb3337df
Bailie Wu <wubailie@gmail.com>
2019-06-02 23:10:50 -0400

    single beer route

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 
 router.use('/search', require('./search'));
 router.use('/auth', require('./auth'));
+router.use('/beer', require('./beer'));
 
 module.exports = router;
