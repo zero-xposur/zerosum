ec76f6a8d8abd5829f7f42595b16819e773c36c5
Bailie Wu <wubailie@gmail.com>
2019-06-02 23:10:13 -0400

    cleaned price model

--- db/models/price.js
+++ db/models/price.js
@@ -11,9 +11,19 @@ const Price = connection.define('price', {
     quantity: Sequelize.TEXT,
 });
 
+// returns cleaned up Tags as an array
 Price.getTagsArray = tags => {
-    return tags.replace(/{|}|"/g, '').split(',');
+    if (typeof tags !== 'string') throw new Error('Tags should be a string.');
+    return Price.getTagsString(tags).split(',');
 };
+
+// returns cleaned up Tags as a string
+Price.getTagsString = tags => {
+    if (typeof tags !== 'string') throw new Error('Tags should be a string.');
+    return tags.replace(/{|}|"/g, '');
+};
+
+console.log('poop');
 
 // COPY public.prices (id, name, image, link, price, tags, description, quantity, "createdAt", "updatedAt") FROM stdin;
 // 33097	Kona Castaway IPA	https://www.totalwine.com/media/sys_master/twmmedia/h51/h29/9687617339422.png	https://www.totalwine.com/beer/ale/ipa-india-pale-ale-/american-ipa/kona-castaway-ipa/p/137098901?s=909&igrules=true	159.990000000000009	{"United States",Hawaii,Ale,"IPA (India Pale Ale)","American IPA"}	Hawaii- American India Pale Ale (IPA)- 6.0% ABV. Kona Castaway, part of Kona's Aloha Series, is a uniquely Hawaiian IPA balanced with delightful tropical fruit notes. It can...	1/2 Keg	2019-05-26 21:10:22.068-04	2019-05-26 21:10:22.068-04
