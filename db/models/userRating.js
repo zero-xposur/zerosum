2e40bfc7be55a34879465805668d90e59b2b541a
Bailie Wu <wubailie@gmail.com>
2019-06-06 21:12:18 -0400

    userRating model and testing

--- db/models/userRating.js
+++ db/models/userRating.js
@@ -13,12 +13,43 @@ User.hasMany(UserRating);
 UserRating.belongsTo(User);
 Babeers.hasMany(UserRating);
 
+// checks for score 0-5, if a user and beer exists.
+// creates or updates a rating
 UserRating.rate = (userId, babeerId, score) => {
-    return UserRating.create({
-        score: score.toFixed(),
-        userId: userId,
-        babeerId: babeerId,
-    });
+    if (score > 5 || score < 0) {
+        throw new Error('Score is not > 0 and < 5');
+    }
+    return User.findByPk(userId)
+        .then(foundUser => {
+            if (!foundUser) throw new Error('No User Found!');
+            return Babeers.findByPk(babeerId);
+        })
+        .then(foundBeer => {
+            if (!foundBeer) throw new Error('No Beer Found!');
+            return UserRating.findOne({
+                where: {
+                    userId: userId,
+                    babeerId: babeerId,
+                },
+            }).then(foundUserRating => {
+                if (foundUserRating) {
+                    return foundUserRating.update({
+                        score: score.toFixed(),
+                    });
+                } else {
+                    return UserRating.create({
+                        score: score.toFixed(),
+                        userId: userId,
+                        babeerId: babeerId,
+                    });
+                }
+            });
+        })
+        .catch(er => {
+            // console.log(er);
+            console.error(er);
+            // throw new Error(er.message);
+        });
 };
 
 module.exports = UserRating;
--- db/postSeed.js
+++ db/postSeed.js
@@ -1,3 +1,6 @@
 const { Babeers, Price, User, UserRating } = require('./models');
 
+// rate(userid, beerid,rating)
 UserRating.rate(1, 2, 3);
+// UserRating.rate(0, 2, 4);
+// UserRating.rate(1, 2, 6);
