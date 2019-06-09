3855b1d6cfbe5cc3909c2d85c9186085013e0fa7
Charlie Azmitia <ccazmitia@gmail.com>
2019-06-09 15:37:28 -0400

    working ocr, needs db search algo

--- src/components/MenuCapture.js
+++ src/components/MenuCapture.js
@@ -10,29 +10,13 @@ import {
 import { Camera } from '@material-ui/icons';
 import { getMenu } from '../reducers';
 import { connect } from 'react-redux';
-import CameraPhoto, { FACING_MODES } from 'jslib-html5-camera-photo';
 
 class MenuCapture extends Component {
-    constructor() {
-        super();
-        // this.cameraPhoto = null;
-        // this.videoRef = React.createRef();
-        this.state = {
-            imgUri: '',
-            // height: 0,
-            // width: 0,
-        };
-    }
-
     setRef = webcam => {
         this.webcam = webcam;
     };
 
     takePhoto = () => {
-        // const config = {
-        //     sizeFactor: 1,
-        // };
-        // console.log(this.state);
         let imgUri = this.webcam.getScreenshot();
         const buffer = Buffer.from(
             imgUri.replace(/^data:image\/\w+;base64,/, ''),
@@ -42,13 +26,10 @@ class MenuCapture extends Component {
         this.props.searchMenu(buffer).then(response => {
             console.log(response);
         });
-        this.setState({ imgUri });
     };
 
     render() {
         const videoConstraints = {
-            // width: 1280,
-            // height: 1920,
             facingMode: 'environment',
         };
 
@@ -82,76 +63,6 @@ class MenuCapture extends Component {
             </Fragment>
         );
     }
-
-    // componentDidMount() {
-    //     this.cameraPhoto = new CameraPhoto(this.videoRef.current);
-    //     this.startCamera(FACING_MODES.ENVIRONMENT, {
-    //         height: 1920,
-    //         width: 1080,
-    //     });
-    // }
-    // componentWillUnmount() {
-    //     this.stopCamera();
-    // }
-    // startCamera(facing, res) {
-    //     console.log(this);
-    //     this.cameraPhoto
-    //         .startCamera(facing, res)
-    //         .then(() => {
-    //             let cameraSettings = this.cameraPhoto.getCameraSettings();
-    //             let { height, width } = cameraSettings;
-    //             this.setState({ height, width });
-    //         })
-    //         .then(() => console.log('camera started'))
-    //         .catch(error => console.error('Camera not started', error));
-    // }
-    // stopCamera() {
-    //     this.cameraPhoto.stopCamera().then(() => console.log('camera stopped'));
-    // }
-    // takePhoto = () => {
-    //     const config = {
-    //         sizeFactor: 1,
-    //     };
-    //     console.log(this.state);
-    //     let imgUri = this.cameraPhoto.getDataUri(config);
-    //     const buffer = Buffer.from(
-    //         imgUri.replace(/^data:image\/\w+;base64,/, ''),
-    //         'base64'
-    //     ).toString('base64');
-    //     console.log(buffer);
-    //     this.props.searchMenu(buffer).then(response => {
-    //         console.log(response);
-    //     });
-    //     this.setState({ imgUri });
-    // };
-    // render() {
-    //     return (
-    //         <Fragment>
-    //             <CssBaseline />
-    //             <Container>
-    //                 <video
-    //                     ref={this.videoRef}
-    //                     autoPlay={true}
-    //                     style={{ width: '100%', height: 'auto' }}
-    //                 />
-    //                 <AppBar
-    //                     position="fixed"
-    //                     color="primary"
-    //                     style={{ top: 'auto' }}
-    //                 >
-    //                     <Toolbar>
-    //                         <IconButton
-    //                             style={{ margin: 'auto' }}
-    //                             onClick={this.takePhoto}
-    //                         >
-    //                             <Camera fontSize="large" />
-    //                         </IconButton>
-    //                     </Toolbar>
-    //                 </AppBar>
-    //             </Container>
-    //         </Fragment>
-    //     );
-    // }
 }
 
 const mapDispatchToProps = dispatch => ({
