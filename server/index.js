c97e3841423eec2e2d8f92b3b90b5253c34fb2f8
Bailie Wu <wubailie@gmail.com>
2019-06-13 00:03:06 -0400

    moving our local test server to httpS://localhost:3000

--- server/index.js
+++ server/index.js
@@ -1,7 +1,17 @@
 const server = require('./server');
-
+const fs = require('fs');
+const https = require('https');
 
 const port = process.env.PORT || 3000;
 
-
-server.listen(port, () => console.log(`listening on port ${port}`));
+// https explanation:
+// https://timonweb.com/posts/running-expressjs-server-over-https/
+https
+    .createServer(
+        {
+            key: fs.readFileSync('server.key'),
+            cert: fs.readFileSync('server.cert'),
+        },
+        server
+    )
+    .listen(port, () => console.log(`listening on port ${port}`));
