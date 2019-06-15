c4b276abb99f2ab04c614a5ad61c3dc127c2a9a5
ami-domadia <adomadia9@gmail.com>
2019-06-15 11:00:26 -0400

    cleanup styling

--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,6 @@ module.exports = {
         test: /\.js$/,
         loader: 'babel-loader',
         exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
       }
     ]
   }
