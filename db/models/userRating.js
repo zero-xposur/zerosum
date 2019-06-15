d0b0fb6ba0b6d91bc27da2b7af9e8908b306c690
Bailie Wu <wubailie@gmail.com>
2019-06-15 14:04:30 -0400

    comments

--- db/models/userRating.js
+++ db/models/userRating.js
@@ -16,6 +16,11 @@ const User = require('./users');
 
 // associations
 
+// User.hasMany(UserRating);
+// UserRating.belongsTo(User);
+// Babeers.hasMany(UserRating);
+// UserRating.belongsTo(Babeers);
+
 // can only review if userRating for that beer & user exists.
 UserRating.review = (userId, babeerId, review) => {};
 
