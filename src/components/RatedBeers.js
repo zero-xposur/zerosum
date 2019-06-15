94fc41a8632d6700ecbb262566086ab400abe99d
Bailie Wu <wubailie@gmail.com>
2019-06-15 15:35:28 -0400

    rated beer page, can se data

--- src/components/RatedBeers.js
+++ src/components/RatedBeers.js
@@ -1,26 +1,42 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import { getBeer } from '../reducers/beer';
-import { getImageUrl } from './utils';
+import { getUserBeerRatings } from '../reducers';
 
-const mapStateToProps = beer => {
-    return { beer };
+const mapStateToProps = state => {
+    return { beerRatings: state.userBeerRatings, user: state.user };
 };
 
 const mapDispatchToProps = dispatch => ({
-    fetchBeer: id => {
-        return dispatch(getBeer(id));
+    fetchBeerRatings: id => {
+        return dispatch(getUserBeerRatings(id));
     },
 });
 
-const RatedBeers = () => {
-    // useEffect(() => {
-    //     fetchBeer(match.params.beerId);
-    // }, []);
-    // let beerPage = beer.singleBeer;
+// https://localhost:3000/api/ratings/6
+
+const RatedBeers = props => {
+    useEffect(() => {
+        console.log('rated props', props);
+        if (props.user) {
+            props.fetchBeerRatings(props.user.id);
+        }
+    }, [props.user]);
+
+    let ratings = props.beerRatings;
+
+    console.log('final props', props);
     return (
         <div>
             <h1>Rated Beers</h1>
+            <ol>
+                {ratings.map(aRating => {
+                    return (
+                        <li key={aRating.id}>
+                            {aRating.babeer.name} My Score: {aRating.score}
+                        </li>
+                    );
+                })}
+            </ol>
         </div>
     );
 };
