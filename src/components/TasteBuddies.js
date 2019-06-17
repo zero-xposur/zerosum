0388942a44cb6c5c659f9af4d6149097c0aed06b
Bailie Wu <wubailie@gmail.com>
2019-06-17 05:02:04 -0400

    boring details from tasteBuddies

--- src/components/TasteBuddies.js
+++ src/components/TasteBuddies.js
@@ -24,13 +24,33 @@ const RatedBeers = props => {
 
     let tasteBuddies = props.tasteBuddies;
 
-    console.log('final props', props);
     return (
         <div>
             <h1>Taste Buddies!</h1>
             <ol>
                 {tasteBuddies.map(user => {
-                    return <li key={user.id}>{user.name}</li>;
+                    return (
+                        <li key={user.id}>
+                            {user.name}
+                            <div />
+                            Correlation:
+                            {(user.correlation * 100).toFixed(2)}% Matches:
+                            {user.precision}
+                            <div />
+                            Their recommended beers for you!
+                            <ol>
+                                {user.bestBeers.map(beer => {
+                                    return (
+                                        <li key={beer.id}>
+                                            {beer.babeer.name} Rating:
+                                            {beer.babeer.score} Their Rating:
+                                            {beer.score}
+                                        </li>
+                                    );
+                                })}
+                            </ol>
+                        </li>
+                    );
                 })}
             </ol>
         </div>
