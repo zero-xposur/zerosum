117a6fc6e7802707e1f9b355a15eaf7cf693345b
EC2 Default User <ec2-user@ip-172-31-44-208.us-east-2.compute.internal>
2019-06-20 19:23:21 +0000

    changed icon

--- src/components/Login.js
+++ src/components/Login.js
@@ -29,7 +29,7 @@ const useStyles = makeStyles(theme => ({
     },
     avatar: {
         margin: theme.spacing(1),
-        backgroundColor: theme.palette.secondary.main,
+        // backgroundColor: theme.palette.secondary.main,
     },
     form: {
         width: '100%',
